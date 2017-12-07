63b7808028f5f59311aa43315b763129dceacd92
Perez white, Justice <JUSTICE_PEREZ_WHITE@homedepot.com>
2017-12-07 16:54:15 -0600

    higher order component solution

--- src/Application.js
+++ src/Application.js
@@ -40,39 +40,49 @@ class PizzaCalculator extends Component {
   }
 }
 
+const WithPizzaCalculations = WrappedComponent => {
+  return class extends Component {
+    state = { ...initialState };
+    
+      updateNumberOfPeople = event => {
+        const numberOfPeople = parseInt(event.target.value, 10);
+        this.setState({ numberOfPeople });
+      };
+    
+      updateSlicesPerPerson = event => {
+        const slicesPerPerson = parseInt(event.target.value, 10);
+        this.setState({ slicesPerPerson });
+      };
+    
+      reset = event => {
+        this.setState({ ...initialState });
+      };
+    
+      render() {
+        const { numberOfPeople, slicesPerPerson } = this.state;
+        const numberOfPizzas = calculatePizzasNeeded(
+          numberOfPeople,
+          slicesPerPerson,
+        );
+    
+        return (
+          <WrappedComponent
+            numberOfPeople={numberOfPeople}
+            updateNumberOfPeople={this.updateNumberOfPeople}
+            slicesPerPerson={slicesPerPerson}
+            updateSlicesPerPerson={this.updateSlicesPerPerson}
+            numberOfPizzas = {numberOfPizzas}
+            reset={this.reset}
+          />
+        );
+      }
+  }
+}
+
+const PizzaContainer = WithPizzaCalculations(PizzaCalculator);
+
 export default class Application extends Component {
-  state = { ...initialState };
-
-  updateNumberOfPeople = event => {
-    const numberOfPeople = parseInt(event.target.value, 10);
-    this.setState({ numberOfPeople });
-  };
-
-  updateSlicesPerPerson = event => {
-    const slicesPerPerson = parseInt(event.target.value, 10);
-    this.setState({ slicesPerPerson });
-  };
-
-  reset = event => {
-    this.setState({ ...initialState });
-  };
-
   render() {
-    const { numberOfPeople, slicesPerPerson } = this.state;
-    const numberOfPizzas = calculatePizzasNeeded(
-      numberOfPeople,
-      slicesPerPerson,
-    );
-
-    return (
-      <PizzaCalculator
-        numberOfPeople={numberOfPeople}
-        updateNumberOfPeople={this.updateNumberOfPeople}
-        slicesPerPerson={slicesPerPerson}
-        updateSlicesPerPerson={this.updateSlicesPerPerson}
-        numberOfPizzas = {numberOfPizzas}
-        reset={this.reset}
-      />
-    );
+    return <PizzaContainer />
   }
 }
